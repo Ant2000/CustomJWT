c9a17ba49b97a296b755ca0ac125e7b548241d1e
Antony <ajosekuruvilla@gmail.com>
2021-07-01 12:01:23 +0530

    Some reorganisation and added examples

---
+++ examples/JWT_Decode/JWT_Decode.ino
@@ -0,0 +1,20 @@
+#include <custom_jwt.h>
+
+char key[] = "tester";
+char string[] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW1wIjoyMi41LCJzcGVlZCI6MjUuMX0.t5CfO7HYM0I1sJknkPyCWctTk6tOVBIU07rjcSULDWI";
+CustomJWT jwt(key);
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println(jwt.decodeJWT(string));
+    Serial.print(jwt.header);
+    Serial.print(" ");
+    Serial.print(jwt.payload);
+    Serial.print(" ");
+    Serial.println(jwt.signature);
+}
+
+void loop()
+{
+}
---
+++ examples/JWT_Encode/JWT_Encode.ino
@@ -0,0 +1,16 @@
+#include <custom_jwt.h>
+
+char key[] = "tester";
+char string[] = "{\"temp\":22.5,\"speed\":25.1}";
+CustomJWT jwt(key);
+
+void setup()
+{
+    Serial.begin(115200);
+    jwt.encodeJWT(string);
+    Serial.println(jwt.out); 
+}
+
+void loop()
+{
+}
